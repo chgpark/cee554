884598b6a4c3d9b44c117380234d4c7af49a3822
chgpark <tytytr2@gmail.com>
2018-10-29 10:55:12 +0900

    train data

--- dataProcess/dataProcess.py
+++ dataProcess/dataProcess.py
@@ -4,18 +4,29 @@ import pandas as pd
 class dataProcess:
 	def __init__(self, numOfMarker):
 		self.cwd = os.getcwd()
+		self.varList = ['X_M', 'Y_M', 'Z_M']
+		self.outDirName = None
+
+	def setProcess(self):
 		self.mkOutFolder()
 		self.varGapDict = {}
-		self.varList = ['X_M', 'Y_M', 'Z_M']
 		self.mkVarGap(numOfMarker)
+
+	def setTrainData(self):
+		self.mkOutFolder()
+		self.uwbList = [1, 2, 3, 4]
 
 	def mkVarGap(self, numOfMarker):
 		for idx in range(numOfMarker):
 			for var in self.varList:
 				self.varGapDict[var+str(idx + 1)] = 0
+						
+	def getVarGap(self, startIdx, endIdx, n):
+		for var in self.varGapDict.keys():
+			self.varGapDict[var] = (self.csvFile[var].iloc[endIdx] - self.csvFile[var].iloc[startIdx]) / (n + 1)
 
 	def mkOutFolder(self):
-		self.outDirPath = os.path.join(self.cwd, "changed")
+		self.outDirPath = os.path.join(self.cwd, self.outDirName)
 		try:
 			os.mkdir(self.outDirPath)
 		except:
@@ -34,15 +45,13 @@ class dataProcess:
 		self.csvOutPath = os.path.join(self.outDirPath, self.csvName)
 		self.csvFile.to_csv(self.csvOutPath)
 
-	def doCSV(self):
+	def doProcess(self):
 		startIdx = 0
 		while self.csvFile['X_M1'].iloc[startIdx] == 0:
 			startIdx += 1
 
 		print(self.csvName)
 		for idx in range(startIdx, self.csvFile.shape[0]):
-			if idx % 100 == 0:
-				print(str(idx) + ' / ' + str(self.csvFile.shape[0]))
 			if self.csvFile['X_M1'].iloc[idx] == 0:
 				if firstFlag:
 					endIdx = idx
@@ -56,18 +65,35 @@ class dataProcess:
 					self.csvFile[var][idx] = self.csvFile[var].iloc[idx - 1] + self.varGapDict[var]
 			else:
 				firstFlag = True
-						
-	def getVarGap(self, startIdx, endIdx, n):
-		for var in self.varGapDict.keys():
-			self.varGapDict[var] = (self.csvFile[var].iloc[endIdx] - self.csvFile[var].iloc[startIdx]) / (n + 1)
 
-	def iterCSV(self):
+	def doTrainData(self):
+		for idx in range(self.csvFile.shape[0]):
+			
+			
+
+	def iterProcess(self):
 		for csvName in self.csvNameList:
 			self.readCSV(csvName)
-			self.doCSV()
+			self.doProcess()
+			self.writeCSV()
+
+	def iterTrainData(self):
+		for csvName in self.csvNameLIst:
+			self.readCSV(csvName)
+			self.doTrainData()
 			self.writeCSV()
 
 if __name__ == "__main__":
-	dataP = dataProcess(5)
+	### data process
+	dataP = dataProcess(5, false)
+	dataP.outDirName = 'changed'
+	dataP.setProcess()
 	dataP.getFolder("181025_proto_data")
-	dataP.iterCSV()
+	dataP.iterProcess()
+
+	### train data
+	dataT = dataProcess(5, true)
+	dataT.outDirName = 'train'
+	dataT.setTrainData()
+	dataT.getFolder("changed")
+	dataT.iterProcess()

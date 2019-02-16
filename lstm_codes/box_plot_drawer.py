0033596fe60e3a84e22555f3844d14bd4f98a6a5
chgpark <cpark@kaist.ac.kr>
2019-02-16 22:10:00 +0900

    edit box_plot_drawer

--- lstm_codes/box_plot_drawer.py
+++ lstm_codes/box_plot_drawer.py
@@ -21,9 +21,9 @@ class BoxPlotModule:
         self.plt = plt
         
         self.target_dir_path = {}
-        self.dict_al = {}
+        self.dict_al = collections.OrderedDict()
         
-        self.dict_error = {}
+        self.dict_error = collections.OrderedDict()
         
         self.list_al_name = None
         self.ticks = []
@@ -39,14 +39,11 @@ class BoxPlotModule:
         
     def load_gt_set(self, dir_path):
         self.gt_dir_path = dir_path
-        self._read_data_from_set_(dir_path)
-        self.dict_gt = self.tmp_dict
-        self.ticks = list(self.dict_gt.keys())
-        
+        self._read_data_from_gt_(dir_path)
+
     def load_al_set(self, dir_path, al_name):
         self.target_dir_path[al_name] = dir_path
-        self._read_data_from_set_(dir_path)
-        self.dict_al[al_name] = self.tmp_dict
+        self.dict_al[al_name] = self._read_data_from_set_(dir_path)
         self.list_al_name = list(self.dict_al)
         
     def get_dict_error(self):
@@ -56,17 +53,23 @@ class BoxPlotModule:
     def _cal_error_per_al_(self, al_name):
         self.dict_error[al_name] = []
         for item in self.ticks:
-            self.dict_error[al_name].append(np.sqrt(np.sum(np.square(self.dict_al[al_name][item] - self.dict_gt[item]), axis=1)).tolist())
+            self.dict_error[al_name].append(np.sqrt(np.sum(np.square(self.dict_al[al_name][item] - self.np_gt), axis=1)).tolist())
+            
+    def _read_data_from_gt_(self, input_path):
+        self.np_gt = np.loadtxt(input_path, delimiter=',')[-4800:,-2:]
+        
         
     def _read_data_from_set_(self, input_path):
-        self.tmp_dict = collections.OrderedDict()
+        tmp_dict = collections.OrderedDict()
         file_list = os.listdir(input_path)
         for file_name in file_list:
             file_path = os.path.join(input_path, file_name)
             if os.path.isdir(file_path):
                 continue
             item_name = os.path.split(file_name)[1].split('.')[0].split('_')[-1]
-            self.tmp_dict[item_name] = np.loadtxt(file_path, delimiter=',')[-4800:,:]
+            tmp_dict[item_name] = np.loadtxt(file_path, delimiter=',')[-4800:,:]
+        self.ticks = list(tmp_dict.keys())
+        return tmp_dict
 
     def _set_box_color_(self, bp, color):
         self.plt.setp(bp['boxes'], color=color)
@@ -92,10 +95,10 @@ class BoxPlotModule:
 
         self.plt.legend()
         self.plt.xlabel("The number of the anchor sensors", fontsize=15)
-        self.plt.ylabel("Error [cm]", fontsize=15)
+        self.plt.ylabel("Error [m]", fontsize=15)
         self.plt.xticks(range(offset, scale*len(ticks), scale), self.ticks)
         self.plt.xlim(-scale/2, scale*len(self.ticks))
-        self.plt.ylim(0, 0.5)
+        self.plt.ylim(0, 0.3)
         self.plt.tight_layout()
         self.plt.savefig('boxcompare.png')
         self.plt.show()
@@ -138,7 +141,7 @@ class DataPlotModule:
 
 if __name__ == '__main__':
     bpm = BoxPlotModule()
-    bpm.do_test('../result_data/fc',['../result_data/stacked_bi', '../result_data/Particle_filter', '../result_data/RONet'])
+    bpm.do_test('RO_test/02-38.csv',['../result_data/Particle_filter','../result_data/fc','../result_data/stacked_bi', '../result_data/RONet'])
     # dpm = DataPlotModule()
     # dpm.plot_data([[3, 4, 1, 5, 9],[2, 3, 1, 6, 10],[0, 2, 5, 4, 1]])
 
